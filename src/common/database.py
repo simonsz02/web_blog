27649314b669cf0c73eff8910d9558acbfd364d4
simon6 <simonszeiler@gmail.com>
2022-02-21 22:45:40 +0100

    changes

---
+++ src/common/database.py
@@ -0,0 +1,24 @@
+import pymongo
+
+
+class Database(object):
+    # local database
+    URI = "mongodb://127.0.0.1:27017"
+    DATABASE = None
+
+    @staticmethod
+    def initialize():
+        client = pymongo.MongoClient(Database.URI)
+        Database.DATABASE = client['fullstack']
+
+    @staticmethod
+    def insert(collection, data):
+        Database.DATABASE[collection].insert_one(data)
+
+    @staticmethod
+    def find(collection, query):
+        return Database.DATABASE[collection].find(query)
+
+    @staticmethod
+    def find_one(collection, query):
+        return Database.DATABASE[collection].find_one(query)
